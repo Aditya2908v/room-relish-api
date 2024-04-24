f2ae2982b191b54696554a0679108c7eaa4d7d9b
40VMadhumithaVM <vmadhu2711@gmail.com>
2024-04-24 14:02:02 +0530

    Added API config for paymnet controller

--- src/main/java/org/example/roomrelish/controllers/PaymentController.java
+++ src/main/java/org/example/roomrelish/controllers/PaymentController.java
@@ -1,5 +1,7 @@
 package org.example.roomrelish.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.example.roomrelish.models.Payment;
 import org.example.roomrelish.services.PaymentServiceImpl;
 import org.jetbrains.annotations.TestOnly;
@@ -18,6 +20,20 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
     //Confirm booking of the room
+    @Operation(
+            description = "Confirm Payment process",
+            summary = "After booking, this payment API confirms the booking by making a payment",
+            responses = {
+                    @ApiResponse(
+                            description = "Details of Payment",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "No booking details found",
+                            responseCode = "204"
+                    )
+            }
+    )
     @PostMapping("/pay/{_bookingId}")
     public ResponseEntity<?> confirmBooking(@PathVariable String _bookingId){
         try{
@@ -28,6 +44,20 @@ public class PaymentController {
             return ResponseEntity.badRequest().body("No booking details found");
         }
     }
+    @Operation(
+            description = "Bookings of an user",
+            summary = "This API returns the bookings that are drafted without payment",
+            responses = {
+                    @ApiResponse(
+                            description = "List of details of booking",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "No bookings found",
+                            responseCode = "204"
+                    )
+            }
+    )
 
     @GetMapping("/myBookings")
     public ResponseEntity<?> myBookings(@RequestParam String _userId) {
